4fca3e6626c43d9eb067eb673ff8b986178936e1
Fyodor <sivkov1998@yandex.ru>
2022-07-12 20:47:38 +0300

    started working on filterPage

---
+++ src/components/PlantsFilter.vue
@@ -0,0 +1,69 @@
+<template>
+    <section class="filter">
+        <h2 class="heading">Попробуем найти что то конкретное?</h2>
+        <div class="inputContainer">
+            <input v-model="filterValue" class="input" placeholder="Что вы ищете?"
+            />
+            <SvgIcons />
+        </div>
+    </section>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import SvgIcons from '@/components/svgIcons.vue'
+
+export default Vue.extend({
+  name: 'plants-filter',
+  data: function () {
+    return {
+      filterValue: ''
+    }
+  },
+  components: {
+    SvgIcons
+  }
+})
+</script>
+<style lang="scss">
+@import '@/variables';
+
+.filter {
+    width: 1280px;
+    padding: 100px 0;
+    align-self: center;
+
+    .heading {
+        @include font(36px, 44px, 500);
+    }
+
+    .inputContainer {
+        position: relative;
+        display: inline-flex;
+        align-items: center;
+        padding-top: 50px - 19px;
+        padding-bottom: 50px;
+        .input {
+            outline: none;
+            border: none;
+            border-bottom: 2px solid #6D6D6D;
+            height: 29px + 38px;
+            width: 717px;
+            background-color: transparent;
+            @include font(24px, 30px, 500);
+            &:focus{
+                border-color: black;
+                &+svg{
+                    path{
+                        fill: black;
+                }
+                }
+            }
+        }
+        svg {
+            position: absolute;
+            right: 0px;
+        }
+    }
+
+}
+</style>
---
+++ src/views/CatalogView.vue
@@ -0,0 +1,23 @@
+<template>
+  <main class="container">
+    <PlantsFilter />
+  </main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import PlantsFilter from '../components/PlantsFilter.vue'
+
+export default Vue.extend({
+  name: 'HomeView',
+  components: {
+    PlantsFilter
+  }
+})
+</script>
+<style lang="scss">
+  @import '@/variables';
+  .container{
+    @include flex(column, center, unset);
+  }
+</style>
